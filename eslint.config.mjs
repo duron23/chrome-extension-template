64108419927deefa49f797d97c5b05c9a8186bd5
Farooq Hussain M <farooq_virgo@live.com>
2024-08-13 21:24:16 +0530

    eslint optimization

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -21,12 +20,12 @@ export default [
   ...compat.extends(
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     "prettier"
   ),
   {
     plugins: {
-      "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": tseslint.plugin,
     },
 
     languageOptions: {
@@ -35,7 +34,10 @@ export default [
         ...globals.node,
         ...globals.mocha,
       },
-      parser: tsParser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+      },
     },
     rules: {
       "@typescript-eslint/no-var-requires": 0,
